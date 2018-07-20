61fdb5ac613f769098715010a64a1f2f45bf12bb
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-07-20 11:58:45 +0300

    clickCounter refactoring

---
+++ src/app/scripts/clickCounter.js
@@ -0,0 +1,20 @@
+const clickCounter = () => {
+  if (typeof Storage !== 'undefined') {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = +localStorage.clickcount + 1;
+      localStorage.setItem(
+        'currentHourResult',
+        +localStorage.getItem('currentHourResult') + 1
+      );
+    } else {
+      localStorage.clickcount = 1;
+      localStorage.setItem('currentHourResult', 1);
+    }
+    const adsReviewed = sessionStorage.getItem('adsReviewed');
+    adsReviewed
+      ? sessionStorage.setItem('adsReviewed', +adsReviewed + 1)
+      : sessionStorage.setItem('adsReviewed', 1);
+  }
+};
+
+export default clickCounter;
