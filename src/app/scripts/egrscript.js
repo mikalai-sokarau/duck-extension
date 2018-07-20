a67fafcac230792310382dba1645f618d3f23329
Mikalai Sokarau <mikalaisokarau@gmail.com>
2018-07-20 13:22:25 +0300

    egrscript refactoring

--- src/app/scripts/egrscript.js
+++ src/app/scripts/egrscript.js
@@ -1,31 +1,20 @@
 if (/http:\/\/egr.gov.by/.test(window.location.href)) {
-    console.log("in Egr");
-    
-    var data;
-    
-    chrome.runtime.sendMessage({greeting: "giveData"}, function(response) {
-        data = response.farewell;
-        console.log(response.farewell);
-        if(data !== undefined) {
-            main();
-        }
-        
-    });
-            
-    function main(){
-     document.getElementsByName("ngrn")[0].value = data;
-        
-        var amount = document.getElementById("txtCaptchaDiv").innerHTML;
-        document.getElementById("txtInput").value = amount;
-        
-        if(sessionStorage.getItem("check") && sessionStorage.getItem("check") == "true"){
-            document.getElementsByName("fndform")[0].submit();
-            sessionStorage.setItem("check", "false");
-        } else if (sessionStorage.getItem("check") == "false") {
-            //doing nothing
-        } else {
-            document.getElementsByName("fndform")[0].submit();
-            sessionStorage.setItem("check", "false");
-        }
+  chrome.runtime.sendMessage({ greeting: 'giveData' }, ({ farewell }) => {
+    if (farewell) {
+      document.getElementsByName('ngrn')[0].value = farewell;
+
+      const amount = document.getElementById('txtCaptchaDiv').innerHTML;
+      document.getElementById('txtInput').value = amount;
+
+      if (sessionStorage.getItem('check') && sessionStorage.getItem('check') == 'true') {
+        document.getElementsByName('fndform')[0].submit();
+        sessionStorage.setItem('check', 'false');
+      } else if (sessionStorage.getItem('check') == 'false') {
+        //doing nothing
+      } else {
+        document.getElementsByName('fndform')[0].submit();
+        sessionStorage.setItem('check', 'false');
+      }
     }
+  });
 }
