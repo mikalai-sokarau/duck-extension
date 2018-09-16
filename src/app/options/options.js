2b5364e9b9546e88b457218c3ddd896a3e865ff4
Mikk <mikalaisokarau@gmail.com>
2018-09-16 19:19:33 +0300

    add description logic to options popup

--- src/app/options/options.js
+++ src/app/options/options.js
@@ -1,3 +1,5 @@
+const icon = '<svg aria-hidden="true" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>';
+
 /* 
   If it is necessary to add more buttons to the options page
   just find a node from 'document' and add it to the 'NODES' object below
@@ -10,6 +12,26 @@ const NODES = {
     descriptionSearch: document.querySelector('#descriptionSearch'),
 }
 const SAVE_MESSAGE_NODE = document.querySelector('.save-msg');
+const searchInput = document.querySelector('#searchInput');
+const searchBtn = document.querySelector('#searchBtn');
+const searchList = document.querySelector('.search-words');
+
+searchBtn.addEventListener('click', () => {
+  const li = document.createElement('li');
+  const liIcon = document.createElement('div');
+  const liText = document.createElement('div');
+  
+  liText.textContent = searchInput.value.toLowerCase();
+  li.appendChild(liText);
+
+  liIcon.classList.add('close-icon');
+  liIcon.innerHTML = icon;
+  li.appendChild(liIcon);
+  
+  searchList.appendChild(li);
+  searchInput.value = '';
+  searchInput.focus();
+})
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
